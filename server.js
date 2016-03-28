f76d7e08dec17329c20629cf133dc5cf571177cc
andersonmcook <andersonmcook@gmail.com>
2016-03-28 18:05:08 -0500

    db name

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const MONGODB_HOST = process.env.MONGODB_HOST || 'localhost'
 const MONGODB_PORT = process.env.MONGODB_PORT || 27017
 const MONGODB_USER = process.env.MONGODB_USER || ''
 const MONGODB_PASS = process.env.MONGODB_PASS || ''
-const MONGODB_DB = 'todos';
+const MONGODB_DB = 'react-todos';
 const MONGODB_URL_PREFIX = MONGODB_USER ? `${MONGODB_USER}:${MONGODB_PASS}@` : ''
 const MONGODB_URL = `mongodb://${MONGODB_URL_PREFIX}${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DB}`
 
@@ -29,14 +29,11 @@ app.use(express.static('public'))
 
 
 app.get('/', todoCtrl.index)
-
 app.get('/api/todos', todoCtrl.loadTodos)
 
 // post routes
 app.post('/api/todos', todoCtrl.createTodo)
-
 app.post('/api/todos/:todoID', todoCtrl.updateTodo)
-
 
 mongoose.connect(MONGODB_URL, (err) => {
   // handle this better
